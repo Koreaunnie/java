44cfe33116d6c4b7a708986a666ad8b3c507938e
koreaunnie <hyunjung1995@gmail.com>
2024-08-14 14:58:46 +0900

    변수명 작성 관습

---
+++ src/ch02/lecture/p01variable/C02Variable.java
@@ -0,0 +1,15 @@
+package ch02.lecture.p01variable;
+
+public class C02Variable {
+    public static void main(String[] args) {
+        // 변수명 작성 관습
+        // 소문자
+        int age;
+
+        // 여러 단어 조합 시 각 단어 앞만 대문자
+        int numberOfStudents; // lowerCamelCase (자바 변수명 관습)
+        int NumberOfStudents; // UpperCamelCase (자바 클래스명 관습)
+        int number_of_students; // sneakCase 자바에선 x (파이썬 st)
+
+    }
+}
