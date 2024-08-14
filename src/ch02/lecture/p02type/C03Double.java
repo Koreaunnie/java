8099cc9b2f6685ef88b8b7411245f2995d37c8e1
koreaunnie <hyunjung1995@gmail.com>
2024-08-14 17:45:33 +0900

    primitive type 띄어쓰기 수정

--- src/ch02/lecture/p02type/C03Double.java
+++ src/ch02/lecture/p02type/C03Double.java
@@ -7,13 +7,12 @@ public class C03Double {
         // 불가
         double a = 0.1;
         double b = 0.2;
-        double c = a + b; // 불가
-
+        double c = a + b; // 오류
         System.out.println("c = " + c);
 
         // 가능한 방법 예시 (각 회사마다 따르는 방법이 있음)
         BigDecimal d = new BigDecimal("0.1");
-        BigDecimal e= new BigDecimal("0.2");
+        BigDecimal e = new BigDecimal("0.2");
         BigDecimal f = d.add(e);
         System.out.println("f = " + f);
 
